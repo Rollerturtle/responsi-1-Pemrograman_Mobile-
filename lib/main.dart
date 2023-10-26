9243b43da038ca1bd269555b0549f283bb0dadb7
Adhwa Moyafi Hartoyo <105986799+Rollerturtle@users.noreply.github.com>
2023-10-26 22:38:56 +0700

    LittleBitChange

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Ikan',
+      title: "Dori's Fish App",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
